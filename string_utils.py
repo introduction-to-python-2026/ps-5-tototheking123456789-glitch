c6303daedd06f4785f2a1bb9276761a70ce8ca9d
YotamHibner <tototheking123456789@gmail.com>
2025-12-08 21:21:12 +0200

    Refactor chemical reaction parsing and atom counting
    Refactor parse_chemical_reaction and count_atoms_in_reaction functions to improve readability and efficiency.

--- string_utils.py
+++ string_utils.py
@@ -27,18 +27,19 @@ def count_atoms_in_molecule(molecular_formula):
     return d
 
 
-
 def parse_chemical_reaction(reaction_equation):
-    """Takes a reaction equation (string) and returns reactants and products as lists.  
-    Example: 'H2 + O2 -> H2O' → (['H2', 'O2'], ['H2O'])"""
-    reaction_equation = reaction_equation.replace(" ", "")  # Remove spaces for easier parsing
+    reaction_equation = reaction_equation.replace(" ", "")  # Remove spaces
     reactants, products = reaction_equation.split("->")
     return reactants.split("+"), products.split("+")
 
+
 def count_atoms_in_reaction(molecules_list):
-    """Takes a list of molecular formulas and returns a list of atom count dictionaries.  
-    Example: ['H2', 'O2'] → [{'H': 2}, {'O': 2}]"""
-    molecules_atoms_count = []
+    total_counts = {}
     for molecule in molecules_list:
-        molecules_atoms_count.append(count_atoms_in_molecule(molecule))
-    return molecules_atoms_count
+        atom_counts = count_atoms_in_molecule(molecule)
+        for atom, count in atom_counts.items():
+            if atom in total_counts:
+                total_counts[atom] += count
+            else:
+                total_counts[atom] = count
+    return total_counts
